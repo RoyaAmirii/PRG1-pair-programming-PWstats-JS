dd9c189a7f67f7b492a65a67f6985b483bc8164f
RoyaAmirii <roya.amiri@ada.ac.uk>
2024-10-22 11:37:50 +0000

    task 1 complete

--- stats.js
+++ stats.js
@@ -1,7 +1,11 @@
-const fs = require("fs");
+const fs = require("fs"); /*This line imports the file system module which provides an API to interact with the 
+file system (reading, writing, updating files, etc.). It allows you to read from and write to files.*/
 
-const inputFile = "10000-common-passwords.csv";
-const outputFile = "statistics.csv";
+const inputFile = "10000-common-passwords.csv"; /*This defines a constant inputFile that holds the name of the file to be read.
+ The file "10000-common-passwords.csv" contains data, in this case, a list of common passwords in CSV 
+ (Comma-Separated Values) format. */
+const outputFile = "statistics.csv"; /*specifies the name of the file where the output (statistics) will be written to.
+ Also be in CSV format.*/
 const delimiter = ",";
 
 function deleteExistingOutputFile() {
@@ -13,11 +17,17 @@ function deleteExistingOutputFile() {
 function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
   const lines = data.split(/\n/);
+  let totalPasswordLength = 0; //Let creates the variable
+ 
 
   for (let line of lines) {
-    elements = line.split(delimiter);
+    const elements = line.split(delimiter);
+    const password = elements [0] //elements of the password(uppercase, lowercase, numbers)
+    const passwordLength = password.length; //this line calculates the length of the extracted password string
     console.log(elements);
+    totalPasswordLength += passwordLength; // Add the password length to the total
   }
+
 }
 
 // Main execution
